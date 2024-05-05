60823e28aa841168066d427f3d8dd690c8298b1c
Parmis Montazerizadeh <138355182+parmismontazeri@users.noreply.github.com>
2024-05-05 19:47:08 +0330

    new things

--- src/components/Buttons.jsx
+++ src/components/Buttons.jsx
@@ -1,7 +1,29 @@
 import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCircle } from "@fortawesome/free-solid-svg-icons";
+import { faCircleCheck } from "@fortawesome/free-solid-svg-icons";
+import { faTrash } from "@fortawesome/free-solid-svg-icons";
+import { useState } from "react";
 
 function Buttons() {
-  return <div></div>;
+  const [done, setDone] = useState(false);
+  return (
+    <div className="flex flex-row justify-between w-full h-8 rounded bg-blue-500 mt-4">
+      <button
+        onClick={() => {
+          setDone(!done);
+        }}
+      >
+        {done ? (
+          <FontAwesomeIcon icon={faCircleCheck} className="text-green-500" />
+        ) : (
+          <FontAwesomeIcon icon={faCircle} />
+        )}
+      </button>
+      <p className={done ? "line-through" : ""}>Task</p>
+      <FontAwesomeIcon icon={faTrash} className="text-red-500" />
+    </div>
+  );
 }
 
 export default Buttons;
--- src/components/ToDoList.jsx
+++ src/components/ToDoList.jsx
@@ -1,18 +1,28 @@
 import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faPlus } from "@fortawesome/free-solid-svg-icons";
+import Buttons from "./Buttons";
 
 const ToDoList = () => {
+  const handleAdd = (e) => {};
   return (
     <div className="h-[15cm] w-[10cm] bg-[#581c87] rounded-[20px]">
       <div className="flex flex-col w-full items-center p-5">
         <h1 className="text-3xl font-bold text-violet-500 ">TO DO LIST</h1>
-        <input
-          name="myInput"
-          placeholder="Task name..."
-          className="rounded-lg h-8 mt-4 pl-2 bg-inherit border-2 border-violet-500"
-        />
-        <FontAwesomeIcon icon={faPlus} />
+        <div className="flex items-center mt-4 gap-3">
+          <input
+            name="myInput"
+            placeholder="Task name..."
+            className="rounded-lg h-8 pl-2 bg-inherit border-2 border-violet-500"
+          />
+          <button
+            onClick={handleAdd}
+            className="bg-green-500 rounded-full w-5 h-5 flex flex-col justify-center"
+          >
+            <FontAwesomeIcon icon={faPlus} />
+          </button>
+        </div>
+        <Buttons />
       </div>
     </div>
   );
